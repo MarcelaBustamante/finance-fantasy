73b71f8a840388947f09a84c700e3b78c6ce59e2
MarcelaBustamante <julbustamante@uade.edu.ar>
2022-07-10 22:27:53 -0300

    char walk with bugs

--- Assets/Scripts/player.cs
+++ Assets/Scripts/player.cs
@@ -10,6 +10,7 @@ public class player : MonoBehaviour
     private RaycastHit2D hitX;
     private RaycastHit2D hitY;
     public float speed;
+    public Animator animator;
 
     private void Start()
     {
@@ -23,20 +24,20 @@ public class player : MonoBehaviour
         float y = Input.GetAxisRaw("Vertical");
         // Reset moveDelta - Esto se reinicia por cada frame
         moveDelta = new Vector3(x,y,0);
-
+        AnimateMovement(moveDelta);
         //Swap sprite direction, wether you're going right or left
         if(moveDelta.x > 0)
             transform.localScale = Vector3.one;
             else if (moveDelta.x < 0)
                 transform.localScale = new Vector3(-1, 1, 1);
         //si queremos que solo se mueva en x e y
-        moveIn4Directions();
+        MoveIn4Directions();
 
         //si queremos incluir las diagonales.
-        //moveIn8Directions();
+        //MoveIn8Directions();
     }
 
-    private void moveIn4Directions()
+    private void MoveIn4Directions()
     {
         // Make sure we can move in this direction, by casting a box first, if the box return null, we are free to move
         hit = Physics2D.BoxCast(transform.position, BoxCollider.size, 0, new Vector2(0, moveDelta.y), Mathf.Abs(moveDelta.y * Time.deltaTime), LayerMask.GetMask("Actor", "Blocking"));
@@ -52,16 +53,32 @@ public class player : MonoBehaviour
             //Hace que se mueve
             transform.Translate(moveDelta.x * Time.deltaTime, 0, 0);
         }
-
     }
 
-    private void moveIn8Directions()
+    private void MoveIn8Directions()
     {
         hitY = Physics2D.BoxCast(transform.position, BoxCollider.size, 0, new Vector2(0, moveDelta.y), Mathf.Abs(moveDelta.y * Time.deltaTime), LayerMask.GetMask("Actor", "Blocking"));
         hitX = Physics2D.BoxCast(transform.position, BoxCollider.size, 0, new Vector2(moveDelta.x, 0), Mathf.Abs(moveDelta.x * Time.deltaTime), LayerMask.GetMask("Actor", "Blocking"));
         if (hitX.collider == null && hitY.collider == null)
         {
             transform.position += moveDelta * Time.deltaTime * speed;
+        }
+    }
+
+    void AnimateMovement(Vector3 direction)
+    {
+        if (animator != null)
+        {
+            if (direction.magnitude > 0)
+            {
+                animator.SetBool("isMoving", true);
+                animator.SetFloat("horizontal", direction.x);
+                animator.SetFloat("vertical", direction.y);
+            }
+            else
+            {
+                animator.SetBool("isMoving", false);
+            }
         }
     }
 }
