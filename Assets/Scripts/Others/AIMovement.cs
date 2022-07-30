a7445dec5666fb88faf1f79894999c014f21e7b4
MarcelaBustamante <julbustamante@uade.edu.ar>
2022-07-30 17:36:35 -0300

    add animals

--- Assets/Scripts/Others/AIMovement.cs
+++ Assets/Scripts/Others/AIMovement.cs
@@ -4,97 +4,72 @@ using UnityEngine;
 
 public class AIMovement : MonoBehaviour
 {
-    public float speed;
+    public float speed = 0.2f;
     public Animator animator;
-    private RaycastHit2D hit;
-    private BoxCollider2D boxCollider;
-    private Vector3 moveDelta;
-    private float randomX;
-    private float randomY;
-    private int flip = 1;
-    private void Start()
+
+    public Vector2 decisionTime = new Vector2(1, 4);
+    internal float decisionTimeCount = 0;
+    internal Vector2[] moveDirections = new Vector2[] {
+        Vector2.right,
+        Vector2.left,
+        Vector2.up,
+        Vector2.down,
+        Vector2.zero,
+        Vector2.zero
+    };
+    internal Vector2 currentMoveDir;
+
+    void Start()
     {
-        boxCollider = GetComponent<BoxCollider2D>();
-        randomX = Random.Range(-1, 1);
-        randomY = Random.Range(-1, 1);
-        PickPosition();
+        decisionTimeCount = Random.Range(decisionTime.x, decisionTime.y);
+        ChooseMoveDirection();
     }
 
-    private void MoveIn4Directions()
+    void Update()
     {
-        // Make sure we can move in this direction, by casting a box first, if the box return null, we are free to move
-        hit = Physics2D.BoxCast(transform.position, boxCollider.size, 0, new Vector2(0, moveDelta.y), Mathf.Abs(moveDelta.y * Time.deltaTime), LayerMask.GetMask("Animal", "Blocking"));
-        if (hit.collider == null)
+        if (decisionTimeCount > 0)
         {
-            moveDelta.y += Mathf.PingPong(Time.time * speed, 1);
-            //Hace que se mueve
-            transform.Translate(0, moveDelta.y * Time.deltaTime, 0);
+            decisionTimeCount -= Time.deltaTime;
         }
+        else
+        {
+            decisionTimeCount = Random.Range(decisionTime.x, decisionTime.y);
+            ChooseMoveDirection();
+        }
+        AnimateMovement();
 
-        hit = Physics2D.BoxCast(transform.position, boxCollider.size, 0, new Vector2(moveDelta.x, 0), Mathf.Abs(moveDelta.x * Time.deltaTime), LayerMask.GetMask("Animal", "Blocking"));
-        if (hit.collider == null)
-        {
-            moveDelta.x += Mathf.PingPong(Time.time * speed, 1);
-            //Hace que se mueve
-            transform.Translate(moveDelta.x * Time.deltaTime, 0, 0);
-        }
     }
-    void AnimateMovement(Vector3 direction)
+
+    void FixedUpdate()
+    {
+        Vector2 delta = currentMoveDir * Time.deltaTime * speed;
+        transform.position += new Vector3(delta.x, delta.y);
+    }
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        currentMoveDir *= -1;
+    }
+
+    void ChooseMoveDirection()
+    {
+        int selected = Mathf.FloorToInt(Random.Range(0, moveDirections.Length));
+        currentMoveDir = moveDirections[selected];
+    }
+
+    void AnimateMovement()
     {
         if (animator != null)
         {
-            if (direction.magnitude > 0)
+            if (currentMoveDir.magnitude > 0)
             {
-                Debug.Log("True");
-                //animator.SetBool("isMoving", true);
-                //animator.SetFloat("horizontal", direction.x);
-                //animator.SetFloat("vertical", direction.y);
+                animator.SetBool("isMoving", true);
+                animator.SetFloat("horizontal", currentMoveDir.x);
+                animator.SetFloat("vertical", currentMoveDir.y);
             }
             else
             {
-                Debug.Log("False");
-                //animator.SetBool("isMoving", false);
+                animator.SetBool("isMoving", false);
             }
         }
-    }
-
-    private void PickPosition()
-    {
-        StartCoroutine(MoveToRandomPos());
-    }
-    IEnumerator MoveToRandomPos()
-    {
-        float i = 0.0f;
-        float rate = 1.0f / speed;
-        moveDelta = new Vector2(transform.position.x * Time.deltaTime, 0);
-
-        while (i < 2.0f )
-        {
-            i += Time.deltaTime * rate;
-            Debug.Log(i);
-            // Make sure we can move in this direction, by casting a box first, if the box return null, we are free to move
-            hit = Physics2D.BoxCast(transform.position, boxCollider.size, 0, new Vector2(moveDelta.x, 0), Mathf.Abs(moveDelta.x * Time.deltaTime), LayerMask.GetMask("Animal", "Blocking"));
-            if (hit.collider != null)
-            {
-                flip = flip * (-1);
-            }
-            if (hit.collider == null)
-            {
-                transform.Translate(moveDelta.x * Time.deltaTime * flip, 0, 0);
-            }
-            yield return null;
-        }
-
-        float randomFloat = Random.Range(0.0f, 1.0f); // Create %50 chance to wait
-        if (randomFloat < 0.5f)
-            StartCoroutine(WaitForSomeTime());
-        else
-            PickPosition();
-    }
-
-    IEnumerator WaitForSomeTime()
-    {
-        yield return new WaitForSeconds(Random.Range(1, 2));
-        PickPosition();
     }
 }
