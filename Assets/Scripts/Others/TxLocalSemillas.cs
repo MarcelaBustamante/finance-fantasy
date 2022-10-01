ca0158b933171f2db9a9fcc8787f277f57ae9497
lucas Bollella <lubolle@gmail.com>
2022-10-01 17:58:44 -0300

    Agrego efecto cross fade al cambiar de scene

--- Assets/Scripts/Others/TxLocalSemillas.cs
+++ Assets/Scripts/Others/TxLocalSemillas.cs
@@ -5,7 +5,8 @@ using UnityEngine;
 public class TxLocalSemillas : Collidable
 {
     public string sceneName;
-
+    public Animator transition;
+    public float transitionTime = 1f;//Tiempo que quiero que tome la transición
     protected override void onCollide(Collider2D coll)
     {
         if(coll.name == "Player" )
@@ -13,8 +14,23 @@ public class TxLocalSemillas : Collidable
             //Guardo el estado actual antes de irme al local de semillas            
             GameManager.instance.SaveState();
             ////Nos vamos al local de semilla, Cargamos la scene
-            UnityEngine.SceneManagement.SceneManager.LoadScene(sceneName);
-         
+            //UnityEngine.SceneManagement.SceneManager.LoadScene(sceneName);
+            StartCoroutine(LoadLevel(sceneName));
+
         }
+    }
+
+    public void LoadNextLevel()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene(sceneName);
+        StartCoroutine(LoadLevel(sceneName));
+    }
+
+    IEnumerator LoadLevel (string sceneName)
+    {
+        transition.SetTrigger("Start");
+        yield return new WaitForSeconds(transitionTime);
+        UnityEngine.SceneManagement.SceneManager.LoadScene(sceneName);
+
     }
 }
